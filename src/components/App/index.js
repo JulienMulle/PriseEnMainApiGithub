80da93ec54afe99e1d52018f52b0ca4efb7f46c7
Julien <julienmulle@gmail.com>
2021-09-15 14:14:49 +0200

    recup de Modele react

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+// == Import npm
+import React from 'react';
+
+// == Import
+
+import './style.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <header>Header</header>
+    <div>SearchBar</div>
+    <div>Message</div>
+    <div>ReposResults</div>
+    
+  </div>
+);
+
+// == Export
+export default App;
