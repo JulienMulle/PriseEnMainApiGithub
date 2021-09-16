837e63d060570cef7877c166651c8bd832905a19
Julien <julienmulle@gmail.com>
2021-09-16 11:22:21 +0200

    gestion de la soumission du formulaire

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
-import { Segment, Input } from 'semantic-ui-react';
+import { Segment, Form } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+
+
 import './style.scss';
 
-function SearchBar() {
+function SearchBar({ onFormSubmit }) {
+    const handleOnSubmit = ()=> {
+            // ici pas besoin de faire event.preventDefault
+    // c'est déjà prévu dans le composant Form de SUI
+    console.log('handleOnSubmit');
+    onFormSubmit('test');
+    }
+
   return (
     <div className="search-bar">
       <Segment>
-        <Input
-          icon="search"
-          iconPosition="left"
-          placeholder="Votre recherche"
-          fluid
-        />
+      <Form onSubmit={handleOnSubmit}>
+          <Form.Input
+            icon="search"
+            iconPosition="left"
+            placeholder="Votre recherche"
+            fluid
+          />
+        </Form>
       </Segment>
     </div>
   );
 }
+
+SearchBar.propTypes = {
+    onFormSubmit: PropTypes.func.isRequired,
+  };
 
 export default SearchBar;
