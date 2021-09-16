e57cbfb65963d3982d202d1288526c03d5bb8c72
Julien <julienmulle@gmail.com>
2021-09-16 15:31:09 +0200

    requete axios + config message

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 // == Import npm
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 
 //components
@@ -8,14 +9,14 @@ import SearchBar from 'src/components/SearchBar';
 import Message from 'src/components/Message';
 import ReposResults from 'src/components/ReposResults';
 
-import reposData from 'src/data/repos';
+
 
 // == Import
 import './style.scss';
 
 // == Composant
 function App() {
-  const trimmedData = reposData.items.map((item) => (
+  const trimGithubData = (items) => items.map((item) => (
   {
     id: item.id,
     description: item.description,
@@ -24,11 +25,44 @@ function App() {
     login: item.owner.login,
   }));
 
-  const [results, setResults] = useState(trimmedData);
+  const [results, setResults] = useState([]);
   // state pour gérer ce qu'il y a dans l'input text
   const [search, setSearch] = useState('');
   // state qui va prendre le nom qu'on placera dans la requête
   const [query, setQuery] = useState('');
+  // state qui prend en charge le total des résultats
+  const [totalCount, setTotalCount] = useState(0);
+
+  const loadData = () => {
+    if (query !==''){
+      axios.get(`https://api.github.com/search/repositories?q=${query}`)
+      .then((response)=> {
+                  // une fois que j'ai reçu je vais utiliser la fonction qui va réduire
+          // le nombre de propriétés attendues pour chaque objet dans le tableau
+          const trimmedData = trimGithubData(response.data.items);
+          setResults(trimmedData);
+          setTotalCount(response.data.total_count);
+      })
+      .catch((error) => console.log(`error`, error));
+    }
+  };
+    // au 1e rendu du composant je teste la requête dans un 1e temps
+  // une fois qu'on a le résultat on le traite pour récupérer
+  // uniquement les propriétés qui nous intéresse,
+  // on vient stocker le tableau d'obejt simplifié dans le state
+  useEffect(loadData, [query]);
+
+  const getMessage = () => {
+    let text = 'Taper votre recherche';
+
+    if (totalCount === 1) {
+      text = 'La recherche a donné un seul résultat';
+    }
+    else if (totalCount > 1) {
+      text = `La recherche a donné ${totalCount} résultats`;
+    }
+    return text;
+  };
 
   return(
   <div className="app">
@@ -38,7 +72,7 @@ function App() {
     inputValue= {search}
     onChangeInputValue={setSearch}
      />
-    <Message content={`La recherche a donné ${reposData.total_count} résultats`}/>
+    <Message content={getMessage()}/>
     <ReposResults results={results} />
   </div>
   );
